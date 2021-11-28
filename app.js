3f621f2b922f4ddca6ee749ef32c58124560723b
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-28 11:57:52 +0100

    make projet morpion

--- app.js
+++ app.js
@@ -1,161 +1,41 @@
-function estValide(button) {
-    return button.innerHTML.length == 0;
-}
+const LEFT_BUTTON = 0;
+const RIGHT_BUTTON = 2;
+const cases = document.getElementsByClassName('case');
+let player = document.getElementById('joueur');
+const reset = document.getElementById('reset');
 
-function setSymbol(btn, symbole) {
-    btn.innerHTML = symbole;
-}
+document.addEventListener('contextmenu', function (event){
+    event.preventDefault();
+});
 
-function rechercherVainqueur(pions, joueurs, tour) {
-    if (
-        pions[0].innerHTML == joueurs[tour] &&
-        pions[1].innerHTML == joueurs[tour] &&
-        pions[2].innerHTML == joueurs[tour]
-    ) {
-        pions[0].style.backgroundColor = "#9ACD32";
-        pions[1].style.backgroundColor = "#9ACD32";
-        pions[2].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[3].innerHTML == joueurs[tour] &&
-        pions[4].innerHTML == joueurs[tour] &&
-        pions[5].innerHTML == joueurs[tour]
-    ) {
-        pions[3].style.backgroundColor = "#9ACD32";
-        pions[4].style.backgroundColor = "#9ACD32";
-        pions[5].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[6].innerHTML == joueurs[tour] &&
-        pions[7].innerHTML == joueurs[tour] &&
-        pions[8].innerHTML == joueurs[tour]
-    ) {
-        pions[6].style.backgroundColor = "#9ACD32";
-        pions[7].style.backgroundColor = "#9ACD32";
-        pions[8].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[0].innerHTML == joueurs[tour] &&
-        pions[3].innerHTML == joueurs[tour] &&
-        pions[6].innerHTML == joueurs[tour]
-    ) {
-        pions[0].style.backgroundColor = "#9ACD32";
-        pions[3].style.backgroundColor = "#9ACD32";
-        pions[6].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[1].innerHTML == joueurs[tour] &&
-        pions[4].innerHTML == joueurs[tour] &&
-        pions[7].innerHTML == joueurs[tour]
-    ) {
-        pions[1].style.backgroundColor = "#9ACD32";
-        pions[4].style.backgroundColor = "#9ACD32";
-        pions[7].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[2].innerHTML == joueurs[tour] &&
-        pions[5].innerHTML == joueurs[tour] &&
-        pions[8].innerHTML == joueurs[tour]
-    ) {
-        pions[2].style.backgroundColor = "#9ACD32";
-        pions[5].style.backgroundColor = "#9ACD32";
-        pions[8].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[0].innerHTML == joueurs[tour] &&
-        pions[4].innerHTML == joueurs[tour] &&
-        pions[8].innerHTML == joueurs[tour]
-    ) {
-        pions[0].style.backgroundColor = "#9ACD32";
-        pions[4].style.backgroundColor = "#9ACD32";
-        pions[8].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-
-    if (
-        pions[2].innerHTML == joueurs[tour] &&
-        pions[4].innerHTML == joueurs[tour] &&
-        pions[6].innerHTML == joueurs[tour]
-    ) {
-        pions[2].style.backgroundColor = "#9ACD32";
-        pions[4].style.backgroundColor = "#9ACD32";
-        pions[6].style.backgroundColor = "#9ACD32";
-        return true;
-    }
-}
-
-function matchNul(pions) {
-    for (var i = 0, len = pions.length; i < len; i++) {
-        if (pions[i].innerHTML.length == 0) return false;
-    }
-
-    return true;
-}
-
-var Afficheur = function(element) {
-    var affichage = element;
-
-    function setText(message) {
-        affichage.innerHTML = message;
-    }
-
-    return { sendMessage: setText };
-};
-
-function main() {
-    var pions = document.querySelectorAll("#Jeu button");
-    var joueurs = ["X", "O"];
-    var tour = 0;
-    var jeuEstFini = false;
-    var afficheur = new Afficheur(document.querySelector("#StatutJeu"));
-    afficheur.sendMessage(
-        "Le jeu peut commencer ! <br /> Joueur " +
-        joueurs[tour] +
-        " c'est votre tour."
-    );
-    for (var i = 0, len = pions.length; i < len; i++) {
-        pions[i].addEventListener("click", function() {
-            if (jeuEstFini) return;
-
-            if (!estValide(this)) {
-                afficheur.sendMessage(
-                    "Case occupée ! <br />Joueur " +
-                    joueurs[tour] +
-                    " c'est toujours à vous !"
-                );
-            } else {
-                setSymbol(this, joueurs[tour]);
-                jeuEstFini = rechercherVainqueur(pions, joueurs, tour);
-
-                if (jeuEstFini) {
-                    afficheur.sendMessage(
-                        "Le joueur " +
-                        joueurs[tour] +
-                        ' a gagné ! <br /> <a href="morpion.html">Rejouer</a>'
-                    );
-                    return;
-                }
-
-
-
-                tour++;
-                tour = tour % 2;
-                afficheur.sendMessage("Joueur " + joueurs[tour] + " c'est à vous !");
+for (let i = 0; i < cases.length ; i++){
+    cases[i].addEventListener('mouseup',function (event){
+        if(event.button === LEFT_BUTTON){
+            if (this.innerHTML ===""){
+                player.innerText = "c'est au tour de joueur O";
+                inserText(this, 'X' ,'green')
             }
-        });
-    }
+        }
+        else if(event.button === RIGHT_BUTTON ){
+            if (this.innerHTML ==="") {
+                player.innerText = "c'est au tour de joueur X";
+                inserText(this, 'O', 'red')
+            }
+        }
+    })
 }
 
-main();
+/*
+ *@param element
+ *@param lettreJoueur
+ *@param classCss
+ */
+function inserText (element, lettreJoueur, classCss) {
+    element.innerText = lettreJoueur;
+    element.classList.add(classCss);
+    element.style.backgroundColor = "#DEDeDe"
+}
+
+reset.addEventListener('click', function (){
+    reset.click();
+})
